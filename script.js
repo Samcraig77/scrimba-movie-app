4e4877c1b7fe41a9db607e4d457c07d2fa9f1d62
R. Sam Craig <SAMCRAIG70@GMAIL.COM>
2025-01-17 18:36:59 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Open movie db API key = 7da6d9bf apikey=7da6d9bf&
+
+const body = document.body
+
+// fetch ('http://www.omdbapi.com/?')
+//     .then( res => res.json())
+//     .then(data => console.log(data))
