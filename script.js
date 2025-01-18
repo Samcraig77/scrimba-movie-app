ac401589676a2e2d302e91b840b3087acc2add6a
R. Sam Craig <SAMCRAIG70@GMAIL.COM>
2025-01-18 14:14:45 -0800

    Boilerplate styling and placeholder finished

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-// Open movie db API key = 7da6d9bf apikey=7da6d9bf&
+// Open movie db API key = 7da6d9bf 
 
-const body = document.body
+const searchForm = document.getElementById('search-form')
+const searchInput = document.getElementById('search-input')
+const searchBtn = document.getElementById('search-btn')
+const mainContent = document.getElementById('main-content')
 
-// fetch ('http://www.omdbapi.com/?')
-//     .then( res => res.json())
-//     .then(data => console.log(data))
+let movieArray = []
+
+searchForm.addEventListener('submit', e => {
+    e.preventDefault()
+    searchMovies(searchInput.value)
+    searchInput.value = ''
+})
+
+function searchMovies(title) {
+    fetch (`http://www.omdbapi.com/?apikey=7da6d9bf&t=${title}`)
+    .then( res => res.json())
+    .then(data => {
+         console.log(data)
+        
+         data.Response === "False" ? mainContent.innerText = `Unable to find what you're looking for. Please try another search` : showMovies(data)
+        })
+    
+}
+
+function showMovies(movie) {
+    mainContent.innerHTML = `
+    <article class="movie-article">
+    <img />
+    <h3>${movie.Title}</h3>
+    </article>`
+}
