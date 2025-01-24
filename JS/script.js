9a213ca6a766c39e8ef2765996e8fb0579569a55
R. Sam Craig <SAMCRAIG70@GMAIL.COM>
2025-01-23 19:21:30 -0800

    Add to watchlist (+) button function added.
    TODO: change API call data saved to local storage to all info returned, not just imdbID

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,18 @@
 // Open movie db API key = 7da6d9bf 
 
+
 const searchForm = document.getElementById('search-form')
 const searchInput = document.getElementById('search-input')
 const searchBtn = document.getElementById('search-btn')
 const mainContent = document.getElementById('main-content')
 const addBtnArray = document.querySelectorAll('.add-btn')
-var existingMovies = JSON.parse(localStorage.getItem("movies"))
+// var existingMovies = JSON.parse(localStorage.getItem("movies"))
 document.addEventListener('click', e => {
     [...e.target.classList] == 'info-btn' ? getPlot(e.target.dataset.imdbid, e.target.parentElement) :
     [...e.target.classList] == 'add-btn' ? addMovieToLocalStorage(e.target.dataset.imdbid) : ''
 })
 
-searchForm.addEventListener('submit', e => {
+searchForm.addEventListener('submit', function() {
     e.preventDefault()
     searchMovies(searchInput.value)
 })
@@ -21,20 +22,26 @@ searchForm.addEventListener('input', e =>
 )
 
 function addMovieToLocalStorage(id) {
+    let currentWatchlist = getCurrentWatchlist()
     const movieObject = {"id": `${id}`}
-    localStorage.setItem("movies" , JSON.stringify(movieObject))
+    currentWatchlist.push(movieObject)
+    currentWatchlist = JSON.stringify(currentWatchlist)
+    localStorage.setItem("movies" , currentWatchlist)
     console.log(JSON.parse(localStorage.getItem("movies")))
+}
+
+function getCurrentWatchlist() {
+    let currentWatchlist = JSON.parse(localStorage.getItem("movies"))
+    !currentWatchlist ? currentWatchlist = [] : ''
+    return currentWatchlist 
 }
 
 function searchMovies(title) {
     fetch (`http://www.omdbapi.com/?apikey=7da6d9bf&s=${title}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
         data.Response === "False" ? mainContent.innerText = `Unable to find what you're looking for. Please try another search`
         : mainContent.innerHTML = showMovies(data.Search)
-
-        console.log(showMovies(data.Search))
     })
     .catch(err => console.error(err))
 }
@@ -53,9 +60,7 @@ function showMovies(arr) {
                 </div>
 
                 <div class="movie-mid">
-                    <p>
-                        <button type="submit" class="add-btn" data-imdbid="${entry.imdbID}" aria-label="Add to watchlist">+</button> Add to Watchlist
-                    </p>
+                        ${addBtnHandler(entry.imdbID)}
                 </div>
 
                 <div class="movie-bottom">
@@ -66,6 +71,24 @@ function showMovies(arr) {
         </article>`).join('')
 }
 
+function addBtnHandler(id) {
+    let currentWatchlist = getCurrentWatchlist()
+    const movieId = currentWatchlist.find(movie => movie.id == id)
+    if (movieId) {
+        return `
+        <p>
+        This movie is on your Watchlist
+        </p>
+        `
+    } else {
+        return `
+        <p>
+            <button type="submit" class="add-btn" data-imdbid="${id}" aria-label="Add to watchlist">+</button>
+            Add to Watchlist
+        </p>`
+    }
+}
+
 function getPlot(id, parent) {
      fetch(`http://www.omdbapi.com/?apikey=7da6d9bf&i=${id}`)
     .then(res => res.json())
@@ -74,11 +97,12 @@ function getPlot(id, parent) {
     })
 }
 
-function displayMovieArray() {
-    console.log(userMovieArray)
-}
+// function displayMovieArray() {
+//     console.log(userMovieArray)
+// }
 
 function handleMoviePoster(poster) {
     poster === 'N/A' ? poster = '/images/placeholder-movie-poster.jpg' : ''
 
     return poster
+}
